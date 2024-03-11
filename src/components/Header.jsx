3f8899e0f7f1907ea98b0f6fe246c37bf0e58864
TofiqMvsm <tofiqmvsm2003@gmail.com>
2024-03-11 20:08:38 +0400

    Final image problem fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import { MdDarkMode, MdLightMode } from "react-icons/md";
 import { Link, useNavigate } from "react-router-dom";
 import { ProjectContext } from "../context/ProjectContext";
 import HamburgerMenu from "./HamburgerMenu";
+import resume from '../assets/resume.pdf'
 const Header = () => {
   const [showBurger, setShowBurger] = useState(window.innerWidth > 768 ? false : true);
 
@@ -65,7 +66,7 @@ const Header = () => {
             Projects
           </Link>
           <a
-          href="./src/assets/resume.pdf"
+          href={resume}
           target="_blank"
             className={`${
               dark ? "nav-link-dark" : "nav-link-light"
--- src/data/skillCard.js
+++ src/data/skillCard.js
@@ -1,50 +1,64 @@
+import image1 from "../assets/images/icons8-html.svg"
+import image2 from "../assets/images/icons8-css.svg"
+import image3 from "../assets/images/icons8-javascript.svg"
+import image4 from "../assets/images/icons8-bootstrap.svg"
+import image5 from "../assets/images/icons8-tailwind-css.svg"
+import image6 from "../assets/images/icons8-react-native.svg"
+import image7 from "../assets/images/icons8-redux.svg"
+import image8 from "../assets/images/icons8-node-js.svg"
+import image9 from "../assets/images/icons8-express-js.svg"
+import image10 from "../assets/images/icons8-mongodb.svg"
+import image11 from "../assets/images/icons8-git.svg"
+import image12 from "../assets/images/icons8-figma.svg"
+
+
 export const skillData = [
   {
-    image: "./src/assets/images/icons8-html.svg",
+    image: image1,
     caption: "HTML",
   },
   {
-    image: "./src/assets/images/icons8-css.svg",
+    image: image2,
     caption: "CSS",
   },
   {
-    image: "./src/assets/images/icons8-javascript.svg",
+    image: image3,
     caption: "JavaScript",
   },
   {
-    image: "./src/assets/images/icons8-bootstrap.svg",
+    image: image4,
     caption: "BootStrap",
   },
   {
-    image: "./src/assets/images/icons8-tailwind-css.svg",
+    image: image5,
     caption: "Tailwind Css",
   },
   {
-    image: "./src/assets/images/icons8-react-native.svg",
+    image: image6,
     caption: "React",
   },
   {
-    image : './src/assets/images/icons8-redux.svg',
+    image : image7,
     caption:"Redux"
   },
   {
-    image: "./src/assets/images/icons8-node-js.svg",
+    image: image8,
     caption: "Node Js",
   },
   {
-    image: "./src/assets/images/icons8-express-js.svg",
+    image: image9,
     caption: "Express Js",
   },
   {
-    image: "./src/assets/images/icons8-mongodb.svg",
+    image:image10,
     caption: "MongoDB",
   },
   {
-    image: "./src/assets/images/icons8-git.svg",
+    image: image11,
     caption: "Git",
   },
   {
-    image: "./src/assets/images/icons8-figma.svg",
+    image:image12,
     caption: "Figma",
   },
   
